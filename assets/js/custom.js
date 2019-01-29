07c678ea4071b1ca43c700ad4a4ca4ee5252e007
Camilo <camilofosco@gmail.com>
2019-01-29 08:25:16 -0500

    adding details

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -33,6 +33,119 @@ var custom = {
         // if (taskIndex == 1) {
         //     hideIfNotAccepted();
         // }
+
+
+        /* HTML5 Video Play sequence of video segments - Working version - Vanilla JS
+        Description: a plain vanilla javascript that makes the most of the HTML5 Video element to play a sequence of video segments continuosly, where src, inp point and out point are specified.
+        Notes: Needs refactoring to remove side effects from methods.
+        */
+
+        //sample sequence of video segments, array of objects with src, in point, and outpoint.
+        // In the mem annotation task, inpoint and outpoint will always be 0 and 3.
+
+        // We should load a set of videos with a given number of repeats and vigilance tasks, and make sure that the videos cover 3 to 5 minutes of content.
+
+        var transcripts = [
+        {"src": "https://www.dropbox.com/s/zvdmd1amf1bcy2r/flickr-0-5-6-10568583056_3.mp4?raw=1", "inPoint": 0, "outPoint":3},
+        {"src": "https://www.dropbox.com/s/yd4pwarjkqr8mcd/flickr-2-6-2-9-2-3-7-2-2526292372_2.mp4?raw=1", "inPoint": 0, "outPoint":3},
+        {"src": "https://www.dropbox.com/s/dtw1n4pr5hi23ki/bulldozer-clears-road-and-military-assist-in-rebuilding-bridge-video-id1B011458_0005.mp4?raw=1", "inPoint": 0, "outPoint":3},
+        {"src": "https://www.dropbox.com/s/evo48ad638e1j95/getty-at-the-police-officers-gesture-the-bulldozer-smashes-its-shovel-into-video-id654455954_31.mp4?raw=1", "inPoint": 0, "outPoint":3},
+        {"src": "https://www.dropbox.com/s/yd4pwarjkqr8mcd/flickr-2-6-2-9-2-3-7-2-2526292372_2.mp4?raw=1", "inPoint": 0, "outPoint":3},
+        {"src": "https://www.dropbox.com/s/cqntd1ngxpn8xm7/flickr-1-7-2-1-3-6-9-0-19617213690_2.mp4?raw=1", "inPoint": 0, "outPoint":3},
+      ]
+
+        //Video instance
+        var video = document.getElementById("video_canvas");
+
+        function playVideoSegments(transcripts){
+        // initialised counter to play video segments in the array
+        var counter = 0;
+        /*******************base case, first video ****************/
+        //base case, playing first video segment in sequence
+        var videoSrc = transcripts[counter]['src'];
+        var inPoint  = transcripts[counter]['inPoint'];
+        var outPoint = transcripts[counter]['outPoint'];
+        // helper function to play one video segment
+        playOneSegment(videoSrc,inPoint,outPoint);
+        /*******************subsequent videos ****************/
+        // for video segments following first one, triggered on play end event listener for HTML5 video
+        video.addEventListener('ended', function(e) {
+            // helper function to play video segments
+              playSqOfSegments(transcripts);
+            }, false);
+        /*******************Helper functions ****************/
+            //define helper method - play sequence of segments
+            function playSqOfSegments(transcripts){
+                    videoSrc = transcripts[counter]['src'];
+                    inPoint  = transcripts[counter]['inPoint'];
+                    outPoint = transcripts[counter]['outPoint'];
+                   playOneSegment(videoSrc,inPoint,outPoint);
+            }
+
+            //define helper method - play one video segment
+            function playOneSegment(videoSrc,inPoint,outPoint){
+                 	video.src = videoSrc;
+                    video.load;
+                    video.currentTime = inPoint;
+                    video.play();
+                    video.ontimeupdate = function() {stopAtOutPoint()};
+                    counter += 1;
+            }
+
+            //define helper method - play video till end point
+            function stopAtOutPoint() {
+                if( parseInt(video.currentTime) > outPoint){
+                    video.pause();
+                    video.currentTime = video.duration;
+                }
+            }
+        }
+
+        //wrap in a function for play button
+        function playVideo(){
+            playVideoSegments(transcripts);
+        }
+
+
+        // HANDLE KEYPRESS (R or Spacebar)
+        document.onkeydown = function(e) {
+          if(e.keyCode == 32 || e.keyCode == 82){
+                document.getElementById("video_canvas").style = "border: 8px solid #76b900";
+            }
+        }
+
+        document.onkeyup = function(e) {
+          if(e.keyCode == 32 || e.keyCode == 82){
+            document.getElementById("video_canvas").style = "";
+            }
+        }
+
+        /************************Buttons*********************/
+        //play
+        var playBtn = document.getElementById('playButton');
+
+        playBtn.onclick = function(e) {
+         playBtn.innerHTML = "Restart";
+            playVideoSegments(transcripts);
+        }
+        //pause
+        var pauseBtn = document.getElementById('pauseButton');
+
+        pauseBtn.onclick = function(e) {
+         pauseBtn.innerHTML = "Pause";
+           console.log(video.src);
+            video.pause();
+        }
+        //resume
+        var resumeButton = document.getElementById('resumeButton');
+
+        resumeButton.onclick = function(e) {
+         resumeButton.innerHTML = "Resume";
+            video.play();
+        }
+
+
+
     },
     collectData: function(taskInput, taskIndex, taskOutput) {
         /*
