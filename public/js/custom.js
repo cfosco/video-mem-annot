3bd88a89ec1e22121fc4ee417a41a6611931c199
Camilo <camilofosco@gmail.com>
2019-02-22 01:26:00 -0500

    End screen revamp

--- public/js/custom.js
+++ public/js/custom.js
@@ -37,6 +37,92 @@ function showTask() {
   var checked = false; // don't check video at end if already checked
   var responses = [];
 
+  function showRadialPercentage() {
+    var margin = 40;
+
+    var wrapper = document.getElementById('score-radial');
+    var start = 0;
+    var end = parseFloat(wrapper.dataset.percentage);
+
+    var colours = {
+      fill: '#' + wrapper.dataset.fillColour,
+      track: '#' + wrapper.dataset.trackColour,
+      text: '#' + wrapper.dataset.textColour,
+      stroke: '#' + wrapper.dataset.strokeColour,
+    }
+
+    var radius = 100;
+    var border = wrapper.dataset.trackWidth;
+    var strokeSpacing = wrapper.dataset.strokeSpacing;
+    var endAngle = Math.PI * 2;
+    var formatText = d3.format('.0%');
+    var boxSize = radius * 2 + margin;
+    var count = end;
+    var progress = start;
+    var step = end < start ? -0.01 : 0.01;
+
+    //Define the circle
+    var circle = d3.svg.arc()
+      .startAngle(0)
+      .innerRadius(radius)
+      .outerRadius(radius - border);
+
+    //setup SVG wrapper
+    var svg = d3.select(wrapper)
+      .append('svg')
+      .attr('width', boxSize)
+      .attr('height', boxSize);
+
+    // ADD Group container
+    var g = svg.append('g')
+      .attr('transform', 'translate(' + boxSize / 2 + ',' + boxSize / 2 + ')');
+
+    //Setup track
+    var track = g.append('g').attr('class', 'radial-progress');
+    track.append('path')
+      .attr('class', 'radial-progress__background')
+      .attr('fill', colours.track)
+      .attr('stroke', colours.stroke)
+      .attr('stroke-width', strokeSpacing + 'px')
+      .attr('d', circle.endAngle(endAngle));
+
+    //Add colour fill
+    var value = track.append('path')
+      .attr('class', 'radial-progress__value')
+      .attr('fill', colours.fill)
+      .attr('stroke', colours.stroke)
+      .attr('stroke-width', strokeSpacing + 'px');
+
+    //Add text value
+    var numberText = track.append('text')
+      .attr('class', 'radial-progress__text')
+      .attr('style','font-size: 30pt;')
+      .attr('fill', colours.text)
+      .attr('text-anchor', 'middle')
+      .attr('dy', '.9rem');
+
+    function update(progress) {
+      //update position of endAngle
+      value.attr('d', circle.endAngle(endAngle * progress));
+      //update text value
+      numberText.text(formatText(progress));
+    }
+
+    (function iterate() {
+      //call update to begin animation
+      update(progress);
+      if (count > 0) {
+        //reduce count till it reaches 0
+        count--;
+        //increase progress
+        progress += step;
+
+        //Control the speed of the fill
+        setTimeout(iterate, 10);
+      }
+    })();
+  }
+
   /**
    * Shows a line graph on the page
    * @param {number[]} yData y values of data points (x is index)
@@ -47,12 +133,20 @@ function showTask() {
    */
   function showGraph(yData, yRange, selector, graphType) {
 
-    var style = graphType == 'Accuracy' ? 'stroke: steelblue' : 'stroke: green';
+    if (graphType == 'Scores') {
+      var style = 'stroke: steelblue;';
+      var fill = ' fill: #679fce;';
+      var suffix = '%';
+    } else {
+      var style = 'stroke: green;';
+      var fill = ' fill: #36a049;'
+      var suffix = '$'
+    }
 
-    var v_width = $(window).width();
+    var v_width = $("#app").width();
     // var v_height = $(window).height();
     // console.log(v_width, v_height)
-    var margin = { top: 20, right: 50, bottom: 30, left: 50 };
+    var margin = { top: 40, right: 50, bottom: 50, left: 50 };
     var width = v_width*0.45 - margin.left - margin.right;
     var height = width - margin.top - margin.bottom;
 
@@ -61,10 +155,10 @@ function showTask() {
     var x = d3.scale.linear().range([0, width]);
     var y = d3.scale.linear().range([height, 0]);
 
-    x.domain([1, data.length]);
+    x.domain([0, data.length+1]);
     y.domain(yRange);
 
-    var xAxis = d3.svg.axis().scale(x).orient("bottom").ticks(data.length);
+    var xAxis = d3.svg.axis().scale(x).orient("bottom").ticks((data.length+1)%15);
     var yAxis = d3.svg.axis().scale(y).orient("left");
 
     var line = d3.svg.line()
@@ -89,10 +183,10 @@ function showTask() {
       .attr("style", style)
       .call(yAxis)
       .append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 6)
+      // .attr("y", 6)
       .attr("dy", ".71em")
-      .style("text-anchor", "end")
+      .attr("x", 5)
+      .style("text-anchor", "start")
       .text(graphType);
 
     svg.append("path")
@@ -101,16 +195,25 @@ function showTask() {
       .attr("style",style)
       .attr("d", line); // 11. Calls the line generator
 
+    svg.selectAll("line-circle")
+      .data(data)
+      .enter().append("circle")
+        .attr("style",style+fill)
+        .attr("r", 4)
+        .attr("cx", function(d) { return x(d[0]); })
+        .attr("cy", function(d) { return y(d[1]); });
+
     var focus = svg.append("g")
       .attr("class", "focus")
       .style("display", "none");
 
     focus.append("circle")
-      .attr("r", 7);
+      .attr("r", 7)
+      .attr("fill-opacity", 0.3)
 
     focus.append("text")
-      .attr("x", 9)
-      .attr("dy", ".35em");
+      .attr("x", 15)
+      .attr("dy", ".55em");
 
     svg.append("rect")
       .attr("class", "overlay")
@@ -128,7 +231,7 @@ function showTask() {
       }
       focus.attr("transform", "translate(" + x(i) + "," + y(data[i - 1][1]) + ")");
       // focus.attr("class", "focus animated infinite zoomIn")
-      focus.select("text").text(data[i - 1][1]);
+      focus.select("text").text(data[i - 1][1]+suffix);
     }
 
   }
@@ -140,16 +243,16 @@ function showTask() {
   function showResultsPage(data) {
     $experiment.css('display', 'none');
     $endGame.css('display', 'flex');
-    $('#repeats-detected').text((data.overallScore * 100).toFixed(0) + '%');
+    $('#score-radial').attr("data-percentage", (data.overallScore * 100).toFixed(0) + '%');
     var pastScores = data.completedLevels.map(function(d) {
       return Math.floor(d.score * 100);
     });
 
     var earnings = [];
     data.completedLevels.reduce(function(a,b,i) { return earnings[i] = a+b.reward; },0);
-
-    showGraph(pastScores, [0,100], '#graph-accuracy', 'Accuracy');
-    showGraph(earnings, [0,earnings[earnings.length-1]+1], '#graph-earnings', 'Earnings');
+    showRadialPercentage();
+    showGraph(pastScores, [0,100], '#graph-accuracy', 'Scores');
+    showGraph(earnings, [0,earnings[earnings.length-1]+1], '#graph-earnings', 'Earnings ($)');
   }
 
   /**
@@ -214,6 +317,7 @@ function showTask() {
     videoElements.push(video);
 
     video.ontimeupdate = function () {
+      // video.currentTime = CLIP_DURATION //DEBUG
       if (video.currentTime >= CLIP_DURATION) {
         // check for missed repeat
         handleCheck(false, false);
@@ -222,7 +326,9 @@ function showTask() {
         video.remove();
         // play next video
         videoElements.shift();
+
         // videoElements = [] // DEBUG
+
         if (videoElements.length > 0) {
           videoElements[0].play();
         } else {
@@ -235,15 +341,21 @@ function showTask() {
             contentType: 'application/json; charset=utf-8',
             dataType: 'json'
           }).done(showResultsPage);
+
+          // DEBUG
           // var data = {};
           // data.overallScore = 0.80
           // data.completedLevels = [{"score":0.3, "reward":1},
-          // {"score":0.4, "reward":1},
+          //
           // {"score":0.44, "reward":1},
           // {"score":0.56, "reward":1},
           // {"score":0.54, "reward":1},
           // {"score":0.7, "reward":1},
-          // {"score":0.83, "reward":1}]
+          // {"score":0.83, "reward":1},
+          // {"score":0.44, "reward":1},
+          // {"score":0.56, "reward":1}
+          // ]
+
           showResultsPage(data)
         }
         // queue up another video
