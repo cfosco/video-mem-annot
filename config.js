dadcdb9b123a71b2ef6af3b8e3873d7ba5573d33
Anelise Newman <apnewman@mit.edu>
2019-02-23 14:37:21 -0500

    config file actually works now

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const env = process.env.NODE_ENV;
+const debug = require('debug')('memento:server');
 
 const dev = {
   dbConfig: {
@@ -24,6 +24,26 @@ const test = {
   errorOnFastSubmit   : true,
 }
 
-const configs = { dev, test }
+// TODO: specify a prod config
+const prod = dev;
 
-module.exports = configs[env];
+const configs = { prod, dev, test }
+
+// NODE_ENV is set by jest to 'test' iff null at start. As such, it cannot be 
+// set by us to specify dev vs prod.
+const node_env = process.env.NODE_ENV;
+// set MEMENTO_ENV to prod, dev, or test
+let memento_env = process.env.MEMENTO_ENV;
+
+// node_env overrides memento_env, for use with jest
+if (node_env) {
+    memento_env = node_env;
+}
+
+if (!(memento_env in configs)) {
+    throw new Error("Please set env var MEMENTO_ENV to one of: prod, dev, test");
+}
+
+debug("Using env", memento_env);
+
+module.exports = configs[memento_env];
