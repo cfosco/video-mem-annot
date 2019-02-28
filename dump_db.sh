18febc371fbd77d6d600419e615314c667d9d7dd
Camilo <camilofosco@gmail.com>
2019-02-28 02:02:21 -0500

    Adding database exploration notebook and compensation hit code to mturk handler

--- dump_db.sh
+++ dump_db.sh
@@ -3,5 +3,5 @@ read -s -p "Password:" p
 
 tables=(users videos levels presentations)
 for t in "${tables[@]}"; do
-    echo 'SELECT * FROM ' "$t" | mysql -B -h mysql.csail.mit.edu -u"$2" -p"$p" "$1" > dump/"$t".tsv
+  echo 'SELECT * FROM ' "$t" | mysql -B -h mysql.csail.mit.edu -u"$2" -p"$p" "$1" > dump/"$t".tsv
 done
