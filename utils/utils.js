9948c81e20a825db4267a19e952fa77731733e6f
Barry Anthony McNamara III <bamazap@hotmail.com>
2019-03-06 10:06:34 -0500

    Document secToHMS function

--- utils/utils.js
+++ utils/utils.js
@@ -1,7 +1,16 @@
+const assert = require('assert');
+
+/**
+ * @param {number} duration in seconds, < 24hrs
+ * @return {string} SQL-legible duration string
+ */
 function secToHMS(duration) {
-  let seconds = parseInt(duration % 60);
-  let minutes = parseInt((duration / 60) % 60);
-  let hours = parseInt((duration / (60 * 60)) % 24);
+  const rawHours = duration / (60 * 60);
+  assert(rawHours < 24, 'Duration must be less than 24 hours.');
+
+  let seconds = Math.floor(duration % 60);
+  let minutes = Math.floor((duration / 60) % 60);
+  let hours = Math.floor(rawHours % 24);
 
   hours = (hours < 10) ? "0" + hours : hours;
   minutes = (minutes < 10) ? "0" + minutes : minutes;
