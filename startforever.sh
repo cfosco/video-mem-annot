1ad37677fba2523fb8ad5da055748eff9939ab90
Anelise Newman <apnewman@csail.mit.edu>
2019-03-31 12:51:25 -0400

    Small change to server start script

--- startforever.sh
+++ startforever.sh
@@ -1,2 +1,3 @@
 source creds.sh;
+npm run build;
 npx forever start -l /data/vision/oliva/scratch/memento-mem-game/logs/forever.log -o /data/vision/oliva/scratch/memento-mem-game/logs/out.log -e /data/vision/oliva/scratch/memento-mem-game/logs/err.log -fa --fifo --uid "memento-prod" bin/www;
